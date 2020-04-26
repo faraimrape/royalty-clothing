9c84b7d5a549d077c52bc65e2c8daaa633583725
Farai Mrape <faraimrape@gmail.com>
2020-04-26 16:55:32 +0200

    create navigation with higher order components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
 function App() {
-  return <HomePage />;
+  return (
+    <div>
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/hats" component={HatsPage} />
+    </div>
+  );
 }
 
 export default App;
